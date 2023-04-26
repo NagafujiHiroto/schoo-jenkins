c64b8697b6697055a51ed21695a01dbe7c569e7f
n-hiro <96487688+NagafujiHiroto@users.noreply.github.com>
2023-04-26 17:56:27 +0900

    Delete compile.sh

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ue -o pipefail
-
-BASE_DIR=$(cd $(dirname $0); pwd)
-cd ${BASE_DIR}
-
-mkdir -p ${BASE_DIR}/target/classes
-javac -cp "${BASE_DIR}/lib/*" -d ${BASE_DIR}/target/classes $(find src/{main,test}/java/schoo/jenkins -name "*.java")
